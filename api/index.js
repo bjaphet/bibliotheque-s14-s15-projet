f4645e9a5074b7f1c983eb8b641f86aa86407fef
bjaphet <bjaphetpiergy@gmail.com>
2026-09-24 13:07:58 +0200

    Avoid Vercel Express entrypoint conflict

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import { app } from '../src/app.js';
+import { app } from '../src/application.js';
 
 export default function handler(request, response) {
 	// Vercel peut transmettre le chemin avec ou sans le préfixe /api.
